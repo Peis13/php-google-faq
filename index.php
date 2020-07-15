b6b7f53355e5d22fc716ccc23b4d3fac6739c2a9
Nicolò <nico.paganelli91@gmail.com>
2020-07-15 15:25:09 +0200

    Struttura 'database'

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
 // Riscrivere questa pagina del sito google: https://policies.google.com/faq
 // Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+
+$faq = [
+  [
+    'domanda' => 'testo domanda',
+    'risposta' => [
+      'paragrafo 1',
+      'paragrafo 2',
+      'paragrafo 3'
+    ],
+  ],
+  [
+    'domanda' => 'testo domanda',
+    'risposta' => [
+      'paragrafo 1'
+    ],
+  ],
+  [
+    'domanda' => 'testo domanda',
+    'risposta' => [
+      'paragrafo 1',
+      'paragrafo 2'
+    ],
+  ],
+];
 ?>
